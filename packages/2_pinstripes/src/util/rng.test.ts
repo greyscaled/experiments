b9006d1dae7d4310029e7173ab1108c31127e56c
Grey Barkans <greybarkans@gmail.com>
2022-12-04 19:19:37 -0500

    allow rng to return floating point

--- packages/2_pinstripes/src/util/rng.test.ts
+++ packages/2_pinstripes/src/util/rng.test.ts
@@ -1,12 +1,11 @@
 import rng from "./rng"
 
 describe("rng", () => {
-    it("returns an integer within (3, 7)", () => {
+    it("returns a float within (3, 7)", () => {
         const min = 3
         const max = 7
         for (let i = 0; i < 100; i++) {
             const got = rng(min, max)
-            expect(Number.isInteger(got)).toBeTruthy()
             expect(got).toBeGreaterThanOrEqual(min)
             expect(got).toBeLessThanOrEqual(max)
         }
--- packages/2_pinstripes/src/util/rng.ts
+++ packages/2_pinstripes/src/util/rng.ts
@@ -1,6 +1,6 @@
 const rng = (min: number, max: number) => {
     const delta = max - min
-    return Math.floor(Math.random() * delta) + min
+    return Math.random() * delta + min
 }
 
 export default rng
